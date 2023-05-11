f98562af56aecbe63976ab9eea6dc05dc3be7671
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-05-11 19:55:34 -0300

    adiciona rascunho usando banco de dados

---
+++ 20230327T162719/sql/03_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO classificador (code, description, valid_from) VALUES (61, 'Recursos Diretamente Arrecadados Vinculados', '2021-06-01');
+
+INSERT INTO execucao (code, dt_doc, vl_doc, hist) VALUES (61, '2021-06-15', 3, 'Recurso para reforma de unidades prisionais');
